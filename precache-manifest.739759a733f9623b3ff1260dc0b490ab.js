267b842cf4cf04e60d3c4987a9ad774083c3ce31
yasmojam <yazzij97@gmail.com>
2020-06-12 11:47:19 +0100

    Updates

---
+++ precache-manifest.739759a733f9623b3ff1260dc0b490ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb13605a13240fbfc6473010d031fa1a",
+    "url": "/MoleculeEditor/index.html"
+  },
+  {
+    "revision": "9655f2d6ab032d127398",
+    "url": "/MoleculeEditor/static/css/main.c5f240a7.chunk.css"
+  },
+  {
+    "revision": "6a0b954fbdb22e349c3e",
+    "url": "/MoleculeEditor/static/js/2.8c220d32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MoleculeEditor/static/js/2.8c220d32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9655f2d6ab032d127398",
+    "url": "/MoleculeEditor/static/js/main.e7b2a37c.chunk.js"
+  },
+  {
+    "revision": "6dd87314851bb93a85f0",
+    "url": "/MoleculeEditor/static/js/runtime-main.0924fdd0.js"
+  }
+]);
